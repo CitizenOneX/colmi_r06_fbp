0fe11265dd97272e7d5ef04cfa6bf19a55409390
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-06-29 12:47:58 +1000

    initial flutter create

---
+++ README.md
@@ -0,0 +1,3 @@
+# colmi_r6_fbp
+
+Reads data from COLMi R6 Smart Ring using flutter_blue_plus
---
+++ android/app/src/main/kotlin/com/example/colmi_r6_fbp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.colmi_r6_fbp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
